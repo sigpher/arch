38936027e0b6bebf7b12a75268365313f9752cc6
sigpher <38996146+sigpher@users.noreply.github.com>
2020-02-28 20:30:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -145,3 +145,187 @@ configrue_networkmanager
 configrue_bootloader
 configure_hostname
 configure_username
+#!/bin/bash
+dhcpcd
+#1.select eufi or mbr
+export INSTALL
+if [ $# = 1 ] &&  [ $1 = "eufi" ];then
+	INSTALL=eufi
+	echo "you will install eufi system"
+else
+	echo "you will install mbr system"
+fi
+#2.part disk
+export DISK
+echo "begin to part disk......"
+echo "parted disk?(G):"
+read  DISK
+echo "root parted(G):"
+read  ROOT
+echo "home parted(G):"
+read  HOME
+dd if=/dev/zero of=/dev/${DISK} seek=1 count=2047 bs=1b
+HOME=`expr ${HOME} + ${ROOT}`G
+ROOT=${ROOT}G
+echo "root part to:${ROOT}"
+echo "home part to:${HOME}"
+if [ ${INSTALL} = "eufi" ];then
+	parted /dev/${DISK} mklabel gpt
+	parted /dev/${DISK} mkpart primary 1M 512M 
+	parted /dev/${DISK} mkpart primary ext4 512M ${ROOT} 
+	parted /dev/${DISK} mkpart primary ext4 ${ROOT} ${HOME} 
+	parted /dev/${DISK} mkpart primary linux-swap ${HOME} 100%
+	mkfs.vfat /dev/${DISK}1
+	mkfs.ext4 /dev/${DISK}2
+	mkfs.ext4 /dev/${DISK}3
+	mkswap /dev/${DISK}4
+	swapon /dev/${DISK}4
+	echo "partting disk all done!!!"
+	#3.mount
+	mount /dev/${DISK}2 /mnt
+	mkdir -p /mnt/boot/efi
+	mount /dev/${DISK}1 /mnt/boot/efi
+	mkdir /mnt/home
+	mount /dev/${DISK}3 /mnt/home
+else
+	parted /dev/${DISK} mklabel msdos
+	parted /dev/${DISK} mkpart primary ext4 1M ${ROOT} 
+	parted /dev/${DISK} mkpart primary ext4 ${ROOT} ${HOME} 
+	parted /dev/${DISK} mkpart primary linux-swap ${HOME} 100%
+	mkfs.ext4 /dev/${DISK}1
+	mkfs.ext4 /dev/${DISK}2
+	mkswap /dev/${DISK}3
+	swapon /dev/${DISK}3
+	echo "partting disk all done!!!"
+	#3.mount
+	mount /dev/${DISK}1 /mnt
+	mkdir /mnt/home
+	mount /dev/${DISK}2 /mnt/home
+fi
+#4.change urt software sources
+echo "begin to change urt software sources......"
+sed -i "1iServer = https://mirrors.tuna.tsinghua.edu.cn/archlinux/\$repo/os/\$arch\n\
+Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch\n\
+Server = https://mirrors.aliyun.com/archlinux/\$repo/os/\$arch\n\
+Server = https://mirrors.163.com/archlinux/\$repo/os/\$arch\n\
+Server = https://mirrors.xjtu.edu.cn/archlinux/\$repo/os/\$arch" /etc/pacman.d/mirrorlist
+echo "changging urt software sources have done!!!"
+#5.install archlinux
+echo "begin to install archlinux......"
+pacstrap /mnt base base-devel
+genfstab -U /mnt >> /mnt/etc/fstab
+cat /mnt/etc/fstab
+echo "installing archlinux have done!!!"
+cp ./arch_root.sh /mnt/
+arch-chroot /mnt
+rm /mnt/arch_root.sh
+if [ ${INSTALL} = "eufi" ];then
+	umount /mnt/boot/efi
+fi
+umount /mnt/home
+umount /mnt
+echo "all have done!!!
+BEGIN TO ENJOY ARCH LINUX!!!"
+#1.set timezone and language
+echo "begin to set timezone and language......"
+#systemdatectl set-timezone Asia/Shanghai
+#systemdatectl set-npt true
+tzselect
+sed -i s/#en_US.UTF-8/en_US.UTF-8/g /etc/locale.gen
+sed -i s/#zh_CN.UTF-8/zh_CN.UTF-8/g /etc/locale.gen
+sed -i s/#zh_CN.GBK/zh_CN.GBK/g /etc/locale.gen
+sed -i s/#zh_CN.GB2312/zh_CN.GB2312/g /etc/locale.gen
+locale-gen
+#2.install grub
+echo "begin to install grub......"
+if [ ${INSTALL} = "eufi" ];then
+pacman -S grub-efi-x86_64 #os-prober
+pacman -S efibootmgr
+grub-install --efi-directory=/boot/efi --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+else
+pacman -S grub #os-prober
+grub-install /dev/${DISK}
+grub-mkconfig -o /boot/grub/grub.cfg
+fi
+echo "installing grub have done!!!"
+#3.install video drive
+echo "begin to install video driver......"
+echo "which one do you want to install?(default install ALL)"
+echo "1)intel"
+echo "2)ati"
+echo "3)nvidia"
+echo "4)all"
+while read -p '#? ' NUM
+do
+	if [ -z "${NUM}" ] || [ ${NUM} -eq 1 ] || [ ${NUM} -eq 2 ] || [ ${NUM} -eq 3 ] || [ ${NUM} -eq 4 ];then
+		break;
+	fi
+done
+case ${NUM} in
+	1)VIDEODRIVER=xf86-video-intel;;
+	2)VIDEODRIVER=xf86-video-ati;;
+	3)VIDEODRIVER=nvidia;;
+	4)VIDEODRIVER="nvidia xf86-video-intel xf86-video-ati";;
+	*)VIDEODRIVER="nvidia xf86-video-intel xf86-video-ati";;
+esac
+pacman -S ${VIDEODRIVER}
+echo "installing video driver have done!!!"
+#install desktop
+echo "begin to install desktop......"
+pacman -S xorg xorg-server
+pacman -S gnome gnome-extra
+systemctl enable gdm
+pacman -S ttf-dejavu wqy-zenhei wqy-microhei
+pacman -S ibus-googlepinyin
+#pacman -S  fcitx-im fcitx-configtool
+#echo 'export GTK_IMMODULE=fcitx\n\
+#export XMODIFIERS="@im=fcitx"\n\
+#export GT_IM_MODULE=fcitx' >>  /etc/bashrc
+#gsettings set \
+#org.gnome.settings-daemon.plugins.xsetting overrides \
+#"{'Gtk/IMModule':<'fcitx'>}"
+pacman -S networkmanager
+echo "installing desktop have done!!!"
+#windows systemfile
+echo "begin to install windows systemfile......"
+pacman -S ntfs-3g dosfstools
+echo "installing windows systemfile have done!!!"
+#yarout
+echo "begin to install yaourt..."
+echo '[archlinuxcn]
+SigLevel = Optional TrustAll
+Server = https://mirrors.ustc.edu.cn/archlinuxcn/$arch' >> /etc/pacman.conf
+pacman -Syu yaourt
+echo "installing yaourt have done"
+#compressed software
+echo "begin to install compressed software......"
+pacman -S p7zip file-roller unrar
+echo "installing compressed software have done!!!"
+#browser
+echo "begin to install browser......"
+pacman -S firefox
+echo "installing browser have done!!!"
+#vim
+echo "begin to install vim"
+pacman -S vim
+echo "vim has been installed!!!"
+#bash-completion
+echo "begin to install bash-completion"
+pacman -S bash-completion
+echo "bash-completion has been installed!!!"
+#others
+pacman -S mlocate
+pacman -S net-tools
+#dhcpcd
+systemctl enable dhcpcd
+#passwd and add user
+echo "begin to set passwd and add user......"
+echo "please input root passwd:"
+passwd
+echo "please add user:"
+read USER
+useradd -m ${USER}
+echo "please input ${USER} passwd:"
+passwd ${USER}
+echo "${USER} ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
